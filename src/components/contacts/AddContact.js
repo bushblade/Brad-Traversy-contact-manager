5163930a4776f4a9aee5abe4343eaf487a62cde5
Will Adams <bushblade@gmail.com>
2018-10-09 17:04:59 +0100

    my version of dry add contact using Object.keys

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Consumer } from '../../context'
+import uuid from 'uuid'
 
 class AddContact extends Component {
   state = {
@@ -8,50 +10,48 @@ class AddContact extends Component {
   }
 
   render() {
-    const { name, email, phone } = this.state
     return (
-      <div className="card mb-3">
-        <div className="card-header">Add Contact</div>
-        <div className="card-body">
-          <form>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                name="name"
-                className="form-control form-control-lg"
-                placeholder="Enter name..."
-                value={name}
-              />
+      <Consumer>
+        {context => (
+          <div className="card mb-3">
+            <div className="card-header">Add Contact</div>
+            <div className="card-body">
+              <form
+                onSubmit={e => {
+                  e.preventDefault()
+                  context.dispatch({
+                    type: 'ADD_CONTACT',
+                    payload: { ...this.state, id: uuid() }
+                  })
+                  this.setState({ name: '', email: '', phone: '' })
+                }}>
+                {Object.keys(this.state).map(field => (
+                  <div className="form-group" key={field}>
+                    <label
+                      htmlFor={field}
+                      style={{ textTransform: 'capitalize' }}>
+                      {field}
+                    </label>
+                    <input
+                      type="text"
+                      name={field}
+                      className="form-control form-control-lg"
+                      placeholder={`Enter ${field}...`}
+                      value={this.state[field]}
+                      onChange={e => this.setState({ [field]: e.target.value })}
+                    />
+                  </div>
+                ))}
+                <input
+                  type="submit"
+                  value="Add Contact"
+                  className="btn btn-block btn-light"
+                />
+              </form>
             </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email"
-                className="form-control form-control-lg"
-                placeholder="Enter email..."
-                value={email}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="phone">Phone</label>
-              <input
-                type="text"
-                name="phone"
-                className="form-control form-control-lg"
-                placeholder="Enter phone..."
-                value={phone}
-              />
-            </div>
-            <input
-              type="submit"
-              value="Add Contact"
-              className="btn btn-block btn-light"
-            />
-          </form>
-        </div>
-      </div>
+          </div>
+        )}
+      </Consumer>
     )
   }
 }
